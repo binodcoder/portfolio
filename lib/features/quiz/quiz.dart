d636f205d69d410510e8e7e6a9ba05c05bf69296
binodcoder <binodcoder@gmail.com>
2025-01-29 09:20:31 +0000

    question screen with shuffled answer

---
+++ lib/features/quiz/quiz.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:untitled/quiz/screens/questions_screen.dart';
+import 'package:untitled/quiz/screens/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  var activeScreen = 'start-screen';
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = 'question-screen';
+    });
+  }
+
+  @override
+  Widget build(context) {
+    final screenWidget = activeScreen == "start-screen" ? StartScreen(switchScreen) : QuestionsScreen();
+
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: screenWidget,
+        ),
+      ),
+    );
+  }
+}
