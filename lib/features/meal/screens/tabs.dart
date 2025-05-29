0e71bc7f1c337f04fefc965d64d9ea64803a88ff
Binod Bhandari <binodcoder@gmail.com>
2025-05-29 08:44:57 +0100

    Connecting multiple providers with each other

--- lib/features/meal/screens/tabs.dart
+++ lib/features/meal/screens/tabs.dart
@@ -4,17 +4,16 @@ import 'package:my_cv/features/meal/screens/filters.dart';
 import 'package:my_cv/features/meal/screens/meals.dart';
 import 'package:my_cv/features/meal/widgets/main_drawer.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:my_cv/features/meal/providers/meals_provider.dart';
 import 'package:my_cv/features/meal/providers/favorites_provider.dart';
 
 import '../providers/filters_provider.dart';
 
-const kInitialFilters = {
-  Filter.glutenFree: false,
-  Filter.lactoseFree: false,
-  Filter.vegetarian: false,
-  Filter.vegan: false,
-};
+// const kInitialFilters = {
+//   Filter.glutenFree: false,
+//   Filter.lactoseFree: false,
+//   Filter.vegetarian: false,
+//   Filter.vegan: false,
+// };
 
 class TabsScreen extends ConsumerStatefulWidget {
   const TabsScreen({super.key});
@@ -47,23 +46,7 @@ class _TabsScreenState extends ConsumerState<TabsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final meals = ref.watch(mealsProvider);
-    final activeFilters = ref.watch(filtersProvider);
-    final availableMeals = meals.where((meal) {
-      if (activeFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
-        return false;
-      }
-      if (activeFilters[Filter.lactoseFree]! && !meal.isLactoseFree) {
-        return false;
-      }
-      if (activeFilters[Filter.vegetarian]! && !meal.isVegetarian) {
-        return false;
-      }
-      if (activeFilters[Filter.vegan]! && !meal.isVegan) {
-        return false;
-      }
-      return true;
-    }).toList();
+    final availableMeals = ref.watch(filteredMealsProvider);
 
     Widget activePage = CategoriesScreen(
       availableMeals: availableMeals,
